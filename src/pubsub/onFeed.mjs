21fcc4e9ef89035e22cdb685adf191ee33545c72
Sergey Kazakov <sergeykazakoff@yandex.ru>
2018-07-22 17:10:12 +0300

    :fire: Fix pubsub onFeed

--- src/pubsub/onFeed.mjs
+++ src/pubsub/onFeed.mjs
@@ -1,18 +1,11 @@
 import xmlParser from "fast-xml-parser"
 import { bot } from "../bot"
 import { User } from "../models/user"
+import { Subscription } from "../models/subscription"
 
 export const onFeed = async ({ topic, feed }) => {
   try {
     const [, channelId] = topic.split("=")
-
-    const message = xmlParser.parse(feed.toString(), {
-      attributeNamePrefix: "",
-      ignoreAttributes: false,
-      allowBooleanAttributes: true,
-    })
-
-    console.log(message)
 
     const {
       feed: {
@@ -20,11 +13,17 @@ export const onFeed = async ({ topic, feed }) => {
           link: { href },
         },
       },
-    } = message
+    } = xmlParser.parse(feed.toString(), {
+      attributeNamePrefix: "",
+      ignoreAttributes: false,
+      allowBooleanAttributes: true,
+    })
+
+    const subscriptions = await Subscription.find({ channelId })
 
     const subscribers = await User.find({
+      _id: { $in: subscriptions.map(({ user }) => user) },
       chatId: { $ne: null },
-      channels: channelId,
     })
 
     await Promise.all(
