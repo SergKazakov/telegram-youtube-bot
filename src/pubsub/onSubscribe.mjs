78da844425cb73ff1e24b8b75032622ed5605817
Sergey Kazakov <sergeykazakoff@yandex.ru>
2018-08-19 08:55:10 +0300

    :fire: Fix expiration timeout

--- src/pubsub/onSubscribe.mjs
+++ src/pubsub/onSubscribe.mjs
@@ -4,5 +4,9 @@ import { handleError } from "../utils/handleError"
 export const onSubscribe = handleError(async ({ topic, lease }) => {
   const [, channelId] = topic.split("=")
 
-  await redis.setex(`channel_id:${channelId}`, lease, new Date(lease * 1000))
+  await redis.setex(
+    `channel_id:${channelId}`,
+    Math.round(lease - Date.now() / 1000),
+    new Date(lease * 1000),
+  )
 })
