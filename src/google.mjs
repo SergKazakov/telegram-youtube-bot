d03cb05446f90f311e542b0ce24a8d8fd2ad797f
Sergey Kazakov <sergeykazakoff@yandex.ru>
2022-11-27 11:16:29 +0300

    :wrench: Remove GOOGLE_REDIRECT_URL

--- src/google.mjs
+++ src/google.mjs
@@ -4,7 +4,7 @@ export const getOauth2Client = () =>
   new googleapis.google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URL,
+    `${process.env.PUBLIC_URL}/oauth2callback`,
   )
 
 export const getYoutubeClient = refreshToken => {
