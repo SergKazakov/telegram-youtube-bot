109e3ce2563fc64e359a46d5a729d68eb26d6dc2
Sergey Kazakov <sergeykazakoff@yandex.ru>
2023-03-18 15:00:56 +0300

    :arrow_up: Update dependencies

--- src/agenda.mjs
+++ src/agenda.mjs
@@ -1,9 +1,8 @@
 import Agenda from "agenda"
 
-import { db } from "./mongodb.mjs"
 import { emitEvent } from "./pubsub/index.mjs"
 
-export const agenda = new Agenda({ mongo: db })
+export const agenda = new Agenda({ db: { address: process.env.MONGODB_URL } })
 
 agenda.on("fail", error => console.error(error))
 
